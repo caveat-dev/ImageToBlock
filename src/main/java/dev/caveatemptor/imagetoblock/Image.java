7937d6d30415202696138340a8d5ad1eee69b04c
caveat <nathan.wignall06@gmail.com>
2023-05-18 16:29:18 -0500

    finished basic drawing

--- src/main/java/dev/caveatemptor/imagetoblock/Image.java
+++ src/main/java/dev/caveatemptor/imagetoblock/Image.java
@@ -8,13 +8,16 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.RasterFormatException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Map;
 
-import static dev.caveatemptor.imagetoblock.LoadImageFromURL.getImage;
 import static java.lang.Integer.parseInt;
 import static java.lang.Math.abs;
 import static org.bukkit.Material.*;
@@ -33,23 +36,6 @@ public class Image implements CommandExecutor {
         BufferedImage img = getImage();
         ArrayList<BufferedImage> imgChunks = new ArrayList<>();
 
-        int imgChunkX = 0;
-        int imgChunkY = 0;
-
-        while (true) {
-            try {
-                imgChunks.add(img.getSubimage(imgChunkX, imgChunkY, 96, 96));
-                imgChunkX += 96;
-                imgChunkY += 96;
-            }
-            catch (RasterFormatException e) {
-                imgChunkX++;
-                imgChunkY++;
-                imgChunks.add(img.getSubimage(imgChunkX, imgChunkY,img.getWidth() - imgChunkX, img.getHeight() - imgChunkY));
-                break;
-            }
-        }
-
         int originX;
         int originY;
         int originZ;
@@ -61,37 +47,48 @@ public class Image implements CommandExecutor {
             return true;
         }
 
-        int placeAtX = originX;
-
-        int placeAtY = originY;
-
         int imgX = 0;
-        int imgY = 0;
-        for (BufferedImage imgChunk : imgChunks) {
+        int imgY = img.getHeight() - 1;
+        for (int y = originY; y < y + img.getHeight() - 1; y++) {
+            for (int x = originX; x < originX + img.getWidth() - 1; x++) {
+                Color pixelColor = new Color(img.getRGB(imgX, imgY));
 
-            for (int i = placeAtY; i < placeAtY + imgChunk.getHeight(); i++) {
-                for (int j = placeAtX; j < placeAtX + imgChunk.getWidth(); j++) {
-                    Color pixelColor = new Color(img.getRGB(imgX, imgY));
+                Material blockToPlace = getBlockClosestInColor(pixelColor);
 
-                    Material blockToPlace = getBlockClosestInColor(pixelColor);
+                plugin.getServer().getWorlds().get(0).getBlockAt(x, y, originZ).setType(blockToPlace);
 
-                    server.getWorlds().get(0).getBlockAt(j, i, originZ).setType(blockToPlace);
-
-                    imgX++;
-                }
-                imgY++;
-                imgX = 0;
+                imgX++;
             }
-            sender.sendMessage(Component.text("Chunk placed!"));
-            placeAtX += imgChunk.getWidth() + 1;
-            if (placeAtX - originX >= img.getWidth()) {
-                placeAtX = originX;
-                placeAtY += imgChunk.getHeight();
-            }
+            sender.sendMessage(Component.text("Row " + (imgY+1) + " placed"));
+            imgY--;
+            imgX = 0;
         }
+
+        sender.sendMessage(Component.text("Finished"));
 
         return true;
     }
+
+
+    private BufferedImage getImage() {
+        URL url = null;
+        try {
+            // url = new URL("https://img.freepik.com/premium-vector/color-spectrum-palette-hue-brightness-black-background-vector-illustration_522680-171.jpg?w=2000");
+            url = new URL("https://cdn.discordapp.com/attachments/1083975218407690350/1108861326941495386/testimage.png");
+        } catch (MalformedURLException ignored) {
+            System.out.println("bad url");
+        }
+
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(url);
+        } catch (IOException e) {
+            System.out.println("some other error");
+        }
+
+        return img;
+    }
+
 
     private Material getBlockClosestInColor(Color pixelColor) {
         Map<String, Object> blockNames = config.getConfigurationSection("blocks").getValues(false);
@@ -105,20 +102,18 @@ public class Image implements CommandExecutor {
             if (!blockNames.containsKey(materialName))
                 continue;
 
-            Map<String, Object> blockColors = config.getConfigurationSection("blocks." + materialName).getValues(false);
-
-            int redDifference;
-            int greenDifference;
-            int blueDifference;
+            Map<String, Object> blockColors;
             try {
-                redDifference = abs((int) blockColors.get("r") - pixelColor.getRed());
-                greenDifference = abs((int) blockColors.get("g") - pixelColor.getGreen());
-                blueDifference = abs((int) blockColors.get("b") - pixelColor.getBlue());
+                blockColors = config.getConfigurationSection("blocks." + materialName).getValues(false);
             }
             catch (IllegalArgumentException e) {
-                System.out.println("Block name " + materialName + " invalid");
+                System.out.println("BLOCK NOT FOUND: " + materialName);
                 continue;
             }
+
+            int redDifference = abs((int) blockColors.get("r") - pixelColor.getRed());
+            int greenDifference = abs((int) blockColors.get("g") - pixelColor.getGreen());
+            int blueDifference = abs((int) blockColors.get("b") - pixelColor.getBlue());
 
             float averageDifference = (redDifference + greenDifference + blueDifference) / 3.0f;
 
