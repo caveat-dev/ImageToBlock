08e971b07ce33b673edbf69629119c28adb8244b
emptor-1 <nathan.wignall06@gmail.com>
2023-05-19 11:33:41 -0500

    efficiency fixes

--- src/main/java/dev/caveatemptor/imagetoblock/ImageCommand.java
+++ src/main/java/dev/caveatemptor/imagetoblock/ImageCommand.java
@@ -1,6 +1,7 @@
 package dev.caveatemptor.imagetoblock;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.command.Command;
@@ -11,13 +12,15 @@ import org.bukkit.configuration.file.FileConfiguration;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static java.lang.Integer.parseInt;
 import static java.lang.Math.abs;
+import static net.kyori.adventure.text.format.NamedTextColor.*;
 
 public class ImageCommand implements CommandExecutor {
     ImageToBlock plugin = ImageToBlock.getInstance();
@@ -31,6 +34,12 @@ public class ImageCommand implements CommandExecutor {
             return true;
 
         BufferedImage img = getImage();
+        List<BufferedImage> imgLayers = new ArrayList<>();
+        // split image into layers
+        for (int y = img.getHeight()-1; y >= 0; y--) {
+            imgLayers.add(img.getSubimage(0, y, img.getWidth()-1, 1));
+        }
+        reverseList(imgLayers);
 
         int originX;
         int originY;
@@ -40,47 +49,42 @@ public class ImageCommand implements CommandExecutor {
             originY = parseInt(args[1]);
             originZ = parseInt(args[2]);
         } catch (Exception ignored) {
+            sendMessage(sender, "Invalid coords", RED);
             return true;
         }
 
-        sender.sendMessage(Component.text(img.getWidth() + " " + img.getHeight()));
+        // buildImage(sender, img, originX, originY, originZ);
 
-        int imgX = img.getWidth() - 1;
-        int imgY = img.getHeight() - 1;
-        for (int y = originY; y < originY + img.getHeight(); y++) {
-            for (int x = originX; x < originX + img.getWidth(); x++) {
-                Color pixelColor = new Color(img.getRGB(imgX, imgY));
-
-                Material blockToPlace = getBlockClosestInColor(pixelColor);
-
-                plugin.getServer().getWorlds().get(0).getBlockAt(x, y, originZ).setType(blockToPlace);
-
-                imgX--;
-            }
-            sender.sendMessage(Component.text("y " + (imgY)));
-            imgY--;
-            imgX = img.getWidth() - 1;
-        }
-
-        sender.sendMessage(Component.text(img.getWidth() + " " + img.getHeight()));
+        imgLayers.forEach((layer) -> {
+            buildLayer(sender, layer, originX, originY + imgLayers.indexOf(layer), originZ);
+        });
+        sender.sendMessage("DONE!");
 
         return true;
     }
 
 
-    private BufferedImage getImage() {
-        URL url = null;
-        try {
-            url = new URL("https://cdn.discordapp.com/attachments/1083975218407690350/1108905658570526900/hi.jpg");
-        } catch (MalformedURLException ignored) {
-            System.out.println("bad url");
-        }
+    private void buildLayer(CommandSender sender, BufferedImage img, int originX, int originY, int originZ) {
+        int imgX = img.getWidth() - 1;
+        for (int x = originX; x < originX + img.getWidth(); x++) {
+            Color pixelColor = new Color(img.getRGB(imgX, 0));
 
+            Material blockToPlace = getBlockClosestInColor(pixelColor);
+
+            server.getWorlds().get(0).getBlockAt(x, originY, originZ).setType(blockToPlace);
+
+            imgX--;
+        }
+    }
+
+
+    private BufferedImage getImage() {
+        File pathToFile = new File("C:\\\\Users\\wn10091617\\Downloads\\images.jpg");
         BufferedImage img = null;
         try {
-            img = ImageIO.read(url);
+            img = ImageIO.read(pathToFile);
         } catch (IOException e) {
-            System.out.println("some other error");
+            System.out.println("failed to get image");
         }
 
         return img;
@@ -92,6 +96,8 @@ public class ImageCommand implements CommandExecutor {
 
         float lowestAverageDifference = 999;
         Material closestBlockInColor = null;
+
+        // TODO: loop through config instead of every material to make things faster
 
         for (Material material : Material.values()) {
 
@@ -121,5 +127,26 @@ public class ImageCommand implements CommandExecutor {
         }
 
         return closestBlockInColor;
+    }
+
+
+    private void sendMessage(CommandSender sender, String message) {
+        sender.sendMessage(Component.text(message));
+    }
+
+    private void sendMessage(CommandSender sender, String message, TextColor color) {
+        sender.sendMessage(Component.text(message).color(color));
+    }
+
+    private static <T> void reverseList(List<T> list)
+    {
+        if (list.size() <= 1)
+            return;
+
+        T value = list.remove(0);
+
+        reverseList(list);
+
+        list.add(value);
     }
 }
